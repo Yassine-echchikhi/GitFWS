2cd4e02ac5c28dd53727ac262bf5722051d977b5
yassine <echchikhi.yassine@gmail.com>
2025-07-29 00:10:08 +0200

    first commit

---
+++ src/FirstPack/PS1.java
@@ -0,0 +1,28 @@
+package FirstPack;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PS1 extends PS {
+
+	@Test
+	public void testRun()
+	{
+		// call the method with object 
+		// PS ps = new PS();
+		// ps.doThis(); 
+		
+		int a = 7;
+		doThis(); // connected to the parent class
+		// Lets create a new object
+		PS2 ps2 = new PS2(a); // Parameterized Constructor
+		System.out.println(ps2.increment());
+		System.out.println(ps2.decrement());
+		
+		//PS3 ps3 = new PS3(a); this line is skipped with inheretience 
+		System.out.println(ps2.multiplyThree());
+		//System.out.println(ps3.multiplyTwo());
+		
+	}
+
+}
